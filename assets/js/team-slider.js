274bf20492482e0cca25e488bfc5978e92ab4175
Quách Phú Thuận <thuanmobile1111@gmail.com>
2025-10-14 22:17:22 +0700

    feat: Build section team

---
+++ assets/js/team-slider.js
@@ -0,0 +1,23 @@
+// Initialize Swiper for the team gallery section
+const swiper = new Swiper('.gallery__swiper', {
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  initialSlide: 1, // Set the second slide as the initial one
+  loop: false, // Do not loop as requested
+  slidesPerView: 'auto',
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 2.5,
+  },
+  pagination: {
+    el: '.gallery__pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
