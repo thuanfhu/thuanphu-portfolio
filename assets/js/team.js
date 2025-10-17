92d4caab511ad2a61b5fd2cac6c46d64026cafed
Quách Phú Thuận <thuanmobile1111@gmail.com>
2025-10-17 19:42:34 +0700

    chore: Add social links

--- assets/js/team.js
+++ assets/js/team.js
@@ -4,28 +4,28 @@ document.addEventListener('DOMContentLoaded', () => {
       name: 'Lê Phạm Thanh Duy',
       desc: 'Người xây dựng giao diện người dùng, tập trung vào trải nghiệm tương tác mượt mà.',
       avatar: './assets/images/team/le-pham-thanh-duy.png',
-      github: '#',
-      website: '#'
+      github: 'https://github.com/duypipi',
+      website: 'https://duypipi.github.io/thanhdui.github.io/'
     },
     {
       name: 'Quách Phú Thuận',
       desc: 'Phụ trách phần logic máy chủ và cơ sở dữ liệu, đảm bảo hệ thống vận hành ổn định.',
       avatar: './assets/images/team/quach-phu-thuan.jpg',
-      github: '#',
-      website: '#'
+      github: 'https://github.com/thuanfhu',
+      website: 'https://thuanfhu.github.io/thuanphu-portfolio/'
     },
     {
       name: 'Lê Thị Mỹ Hậu',
       desc: 'Hiện thực hóa các bản thiết kế, mang lại giao diện trực quan và thân thiện.',
       avatar: './assets/images/team/le-thi-my-hau.jpg',
-      github: '#',
+      github: 'https://github.com/annahwork',
       website: '#'
     },
     {
       name: 'Đỗ Kim Đại',
       desc: 'Đảm bảo trang web hiển thị tốt và hoạt động hiệu quả trên mọi thiết bị.',
       avatar: './assets/images/team/do-kim-dai.png',
-      github: '#',
+      github: 'https://github.com/AyBi248',
       website: '#'
     }
   ];
